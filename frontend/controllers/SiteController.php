02eb48beda209bf4138c44357a2bfb4089c15506
HYQ <yaqianghan_827@163.com>
2018-05-04 10:17:23 +0800

    first  commit

---
+++ frontend/controllers/SiteController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? '1234' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
